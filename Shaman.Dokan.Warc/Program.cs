6b680bca58f6fbdfc7eaa9ae04371ae7ae4c91a6
Andrea Martinelli <andmartinelli@gmail.com>
2017-03-30 16:41:29 +0200

    Support for .cdx.gz, fixed CDX parsing error

--- Shaman.Dokan.Warc/Program.cs
+++ Shaman.Dokan.Warc/Program.cs
@@ -10,9 +10,9 @@ namespace Shaman.Dokan
         static int Main(string[] args)
         {
             var file = args.FirstOrDefault(x => !x.StartsWith("-"));
-            if (file == null || !file.EndsWith(".cdx"))
+            if (file == null || !(file.EndsWith(".cdx") || file.EndsWith(".cdx.gz")))
             {
-                Console.WriteLine("Must specify a .cdx file.");
+                Console.WriteLine("Must specify a .cdx or .cdx.gz file.");
                 return 1;
             }
             var mountpoint = new WarcFs(file).MountSimple(4);
